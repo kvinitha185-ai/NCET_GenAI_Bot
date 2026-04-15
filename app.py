9f8c36fafbb2d2f0bc76ccc5fbd4da8cb82b8e10
kvinitha185-ai <kvinitha185@gmail.com>
2026-04-15 11:56:36 +0530

    Implement content generation and download feature
    Added functionality to generate marketing content based on user input for product and audience, and enabled downloading of the generated content as a TXT file.

--- app.py
+++ app.py
@@ -4,3 +4,32 @@ from groq import Groq
 st.set_page_config("pragyanAI Content Generator", layout="wide")
 st.title("pragyan_AI - Content Generator")
 st.image("pragyan_AI_WhatsApp Image 2026-02-22 at 6.16.39 PM (1).jpeg")
+ #Get GROQ API Key
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+
+# Get Product Name and Audience for That Product
+product = st.text_input("Product")
+audience = st.text_input("Audience")
+
+# Button to Generate Content
+if st.button("Generate Content"):
+    prompt = f"Write marketing content for {product} targeting {audience}."
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    st.session_state.text = response.choices[0].message.content
+    text =response.choices[0].message.content
+    st.write(text)
+
+# After Content Create - Download The File
+if "text" in st.session_state:
+    content = st.text_area("Generated Content", st.session_state.text, height=300)
+    st.download_button(
+            label="⬇️ Download as TXT",
+            data=content,
+            file_name="marketing_copy.txt",
+            mime="text/plain"
+        )
+else:
+    st.info("Generate content first")
