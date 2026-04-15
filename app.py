076efeecb484746381b91e9816f4afc5edb74d73
kvinitha185-ai <kvinitha185@gmail.com>
2026-04-15 11:10:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-streamlit
-groq
-google-auth
-google-auth-oauthlib
-google-api-python-client
+import.streamlit as st
+from groq import Groq
+
+st.set_page_config("pragyanAI Content Generator", layout="wide")
+st.title("pragyan_AI - Content Generator")
+st.image("pragyan_AI_WhatsApp Image 2026-02-22 at 6.16.39 PM (1).jpeg")
